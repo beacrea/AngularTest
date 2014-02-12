67f84673c1bb10352e428d7e65ed48ab5e0adb9a
Coty Beasley <design@beasleycreative.com>
2014-02-12 17:07:03 -0600

    Changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-// Angular Controller
-function FirstCtrl($scope) {
-	$scope.data = {message: 'panel alert-success'};
+/* ==========================================================================
+   App Functions
+   ========================================================================== */
+
+// Calculate Weekly Spending
+function wkTotalSpending() {
+    for (var property in budget) {
+        var transactionTotal = 0;
+        for (var i=0; i<budget[property].transactions.length; i++) {
+            transactionTotal += budget[property].transactions[i];
+        }
+        budget[property].wkTotalSpent = transactionTotal;
+    }
+}
+
+// Determine Budget Remaining
+function detBudgetRemain() {
+    for (var property in budget) {
+        budget[property].budgetRemain = budget[property].budgetStart - budget[property].wkTotalSpent;
+    }
 }
